9facaadf0f7931476fa8895b440d6fe542285b7b
GangOn0215 <ekdldktnekf@gmail.com>
2022-04-23 22:32:59 +0900

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { sequelize } = require("./database/models/index");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const router = require("./router/index");
+
+sequelize
+  .sync()
+  .then(() => console.log("connected database"))
+  .catch((err) => console.log("occurred error in database connecting", err));
+
+app.listen(port, () => {
+  console.log(`server is on ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("hi friend");
+});
+
+app.use(router);
